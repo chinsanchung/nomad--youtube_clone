2de7436e391bdd0fadbfc01d522842797b5442d1
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-16 23:48:59 +0900

    update: res.locals.user 수정

--- middlewares.js
+++ middlewares.js
@@ -1,12 +1,12 @@
 import routes from "./routes";
 
 export const localsMiddleware = (req, res, next) => {
-    res.locals.siteName = "Youtube";
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1,
-    };
+  res.locals.siteName = "Youtube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1,
+  };
 
-    next();
+  next();
 };
