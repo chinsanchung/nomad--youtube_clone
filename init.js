37ebb66bbdd764d2b7af241b7fa1c167a51c4f6a
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-24 10:07:14 +0900

    update: Model 들을 import

--- init.js
+++ init.js
@@ -1,10 +1,12 @@
-import "./models/Video";
-import "./models/Comment";
 import dotenv from "dotenv";
 import app from "./app";
 import "./db";
 
 dotenv.config();
+
+import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
